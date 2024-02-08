2153994837c20415bf4a53e683ab629c93edf3a2
YongWu <43053906+YongWu-cs@users.noreply.github.com>
2024-02-09 00:42:47 +0100

    quetion type model

---
+++ Question_type/model.py
@@ -0,0 +1,34 @@
+from transformers import BertForSequenceClassification
+import torch
+device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class BERTMultiLabelBinaryClassification(torch.nn.Module):
+    def __init__(self, num_labels):
+        super(BERTMultiLabelBinaryClassification, self).__init__()
+        self.bert = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=num_labels)
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+        if labels is not None:
+            pos_weight = torch.tensor([0.161380,0.758311,0.039626,0.924899,0.042553,0.130641])  # 根据需要调整权重
+            pos_weight.to(device)
+            loss_fct = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+            loss_fct.to(device)
+            loss = loss_fct(logits.view(-1, self.bert.config.num_labels), labels.view(-1, self.bert.config.num_labels))
+            return loss
+        else:
+            return logits
+
+# class BERTMultiLabelBinaryClassification(torch.nn.Module):
+#     def __init__(self, num_labels):
+#         super(BERTMultiLabelBinaryClassification, self).__init__()
+#         self.bert = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=num_labels)
+#         self.sigmoid = torch.nn.Sigmoid()
+
+#     def forward(self, input_ids, attention_mask, labels=None):
+#         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+#         logits = outputs.logits
+#         probas = self.sigmoid(logits)
+#         return probas
+
+# model = BERTMultiLabelBinaryClassification(num_labels=len(labels[0]))
