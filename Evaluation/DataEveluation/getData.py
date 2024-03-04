2ee770d3254417efc791a7fb5767aea15af467d9
lingyoumax <54953768+lingyoumax@users.noreply.github.com>
2024-03-04 19:00:53 +0800

    Delete Evaluation directory

--- Evaluation/DataEveluation/getData.py
+++
@@ -1,8 +0,0 @@
-import json
-def getData(file_path = 'dataset/dataset_SFTModel.json'):
-    
-    # 以读取模式打开文件
-    with open(file_path, 'r') as file:
-        # 使用json.load()读取文件内容
-        data = json.load(file)
-    return data
