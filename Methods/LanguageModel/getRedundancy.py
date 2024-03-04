6dc67a59fdf961e308c6116611cfe015f414ad5b
lingyoumax <54953768+lingyoumax@users.noreply.github.com>
2024-03-04 19:12:04 +0800

    Delete Methods/LanguageModel directory

--- Methods/LanguageModel/getRedundancy.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-def getRedundancy(answer):
-    summary = summarizer(answer, max_length=len(answer), min_length=2, do_sample=False)
-
-    return len(summary[0]['summary_text'].split()) / len(answer.split())
